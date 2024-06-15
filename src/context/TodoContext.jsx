456f09803c0090045a5807b90915a73c1038fd11
José Carlos <josecarlospaivasantos2001@gmail.com>
2024-06-15 14:55:37 -0300

    .

---
+++ src/context/TodoContext.jsx
@@ -0,0 +1,48 @@
+//Gerencia o estado global da aplicação, fornecendo funções para adicionar, atualizar e deletar todos.
+import {createContext, useState } from "react"
+
+// Criação do contexto
+export const TodoContext = createContext()
+
+// Componente provedor do contexto
+export function TodoProvider({children}){
+
+    //A linha const [todos, setTodos] = useState([]) utiliza o hook useState do React para definir um estado local dentro de um componente funcional.
+    //Ele retorna um par de valores: o estado atual e uma função para atualizá-lo.
+    const [todos, setTodos] = useState([])
+
+    // Função para adicionar um novo todo
+    const addTodo = (text) => {
+        // A sintaxe [...todos] cria uma nova matriz
+        // contendo todos os itens atuais do estado todos.
+        // Isso é conhecido como "spread operator" e é uma maneira de copiar todos os elementos de uma matriz para outra.
+        setTodos([...todos, { id: Date.now(), text, completed: false }])
+    }
+
+    // Função para atualizar um todo existente
+    const updateTodo = (id, newText) => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, text: newText } : todo))
+    }
+
+    // Função para deletar um todo
+    const deleteTodo = (id) => {
+        // A função filter é chamada na matriz todos.
+        // A função filter cria uma nova matriz contendo apenas os elementos que satisfazem a condição fornecida.
+        // Neste caso, a condição é que o id do todo deve ser diferente do id passado como argumento.
+        setTodos(todos.filter(todo => todo.id !== id))
+    }
+
+    return (
+        // TodoContext.Provider é um componente que faz parte do contexto criado anteriormente.
+        // Ele aceita um valor (value) que será disponibilizado para todos os componentes filhos que consomem o contexto.
+        <TodoContext.Provider value={{todos, addTodo, updateTodo, deleteTodo}}>
+            {children}
+        </TodoContext.Provider>
+    )
+}
+
+// todos: O estado atual das tarefas, um array de objetos.
+// addTodo: Função para adicionar uma nova tarefa.
+// updateTodo: Função para atualizar uma tarefa existente.
+// deleteTodo: Função para deletar uma tarefa.
+// children é uma prop especial no React que representa os elementos filhos passados para este componente.
